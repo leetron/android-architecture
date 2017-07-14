166a05132860a4ac4c3dcdec7e2d3faec5c67836
Tai Nguyen Cao <tai.nguyen@hubcba.com>
2017-07-14 16:29:04 +0700

    + init

---
+++ AndroidArchitecture/app/src/main/java/tisoul/dev/androidarchitecture/presentation/welcome/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package tisoul.dev.androidarchitecture.presentation.welcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import tisoul.dev.androidarchitecture.R;
+
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
