c488fbda0711b00d894bec30ba2ff0b332bb3d94
Tai Nguyen Cao <tai.nguyen@hubcba.com>
2017-07-14 18:07:41 +0700

    + implement presentation layer

--- AndroidArchitecture/app/src/main/java/tisoul/dev/androidarchitecture/WelcomeActivity.java
+++ AndroidArchitecture/app/src/main/java/tisoul/dev/androidarchitecture/WelcomeActivity.java
@@ -1,7 +1,11 @@
 package tisoul.dev.androidarchitecture;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import tisoul.dev.mvp.ProfileActivity;
 
 public class WelcomeActivity extends Activity {
 
@@ -9,5 +13,19 @@ public class WelcomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        findViewById(android.R.id.content)
+                .setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                startActivity(
+                                        new Intent(
+                                                view.getContext(),
+                                                ProfileActivity.class)
+                                );
+                            }
+                        }
+                );
     }
 }
